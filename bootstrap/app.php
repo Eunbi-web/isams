5e5ce3bf19d60b311c266ee30e9672a5ec580df9
Eunbi <jeffersonlibradilla708@gmail.com>
2026-08-28 11:54:13 +0800

    bootstrap changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,37 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
+
+$app = Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up'
+    )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(['role' => \App\Http\Middleware\RoleMiddleware::class]);
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {})->create();
+    ->withExceptions(function (Exceptions $exceptions) {})
+    ->create();
+
+/*
+|--------------------------------------------------------------------------
+| Vercel writable storage
+|--------------------------------------------------------------------------
+*/
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $tmpStorage = '/tmp/laravel';
+
+    if (!is_dir($tmpStorage)) {
+        mkdir($tmpStorage, 0755, true);
+    }
+
+    $app->useStoragePath($tmpStorage);
+}
+
+return $app;
